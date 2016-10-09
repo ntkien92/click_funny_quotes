12afa98a936a7c8ecb5e12defb922bfdf1d93640
Nguyen Trung Kien <kiennt@asiantech.vn>
2016-10-09 15:29:48 +0700

    add layout

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,4 @@
 class HomesController < ApplicationController
   def index
-    render 'test/index_1'
   end
 end
--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,5 +1,5 @@
 class TestController < ApplicationController
   def index
-    
+    render 'test/test_3'
   end
 end
